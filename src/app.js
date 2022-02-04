efb4a1df4008c20ed1715422f72083930a9714a3
jehison98 <jehison-98@hotmail.com>
2022-02-03 20:09:25 -0600

    v1.0.2 cash machine fixed some bugs

--- src/app.js
+++ src/app.js
@@ -18,10 +18,7 @@ app.use(express.static(path.resolve(__dirname, "public")));
 //middlewares
 app.use(morgan("dev"));
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://cash-machine-omnidoc.herokuapp.com/"
-  ); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
